dec3c02191e27a1aaa04882f077baa15fb67ef6c
f-hy <friendy2021@outlook.com>
2026-05-22 03:18:39 +0800

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+'''
+@file main.py
+@author f-hy (friend0@qq.com)
+@date 2026-05-22
+@version 0.1
+@brief 
+@github https://github.com/f-hy
+@copyright Copyright (c) 2026
+'''
+def main():
+  print("Hello from deltaforcetool!")
+if __name__ == "__main__":
+  main()
